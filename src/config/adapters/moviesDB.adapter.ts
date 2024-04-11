9985d35c3fd269f8ef51c5ea3451b70f533f76af
Ariel Verdugo <arielverdugo@MacBook-Pro-de-Ariel-2.local>
2024-04-11 17:31:47 -0300

    Added environment variables

--- src/config/adapters/moviesDB.adapter.ts
+++ src/config/adapters/moviesDB.adapter.ts
@@ -1,10 +1,11 @@
+import { THE_MOVIE_DB_KEY } from "@env";
 import { AxiosAdapter } from "./http/axios.adapter";
 
 
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: '1d1e3eb34d70d120d43954cd81f5915d',
+        api_key: THE_MOVIE_DB_KEY,
         language: 'es'
     }
 })
